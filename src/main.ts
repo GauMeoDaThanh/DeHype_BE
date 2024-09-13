fd5c71cbf87f92d7fdc010697276fdd40061a477
Van Dat <ddat888@gmail.com>
2024-09-13 21:35:58 +0700

    chore: initial project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configServer = app.get(ConfigService);
+  const port = configServer.get('PORT');
+
+  app.setGlobalPrefix('api/v1', { exclude: [''] });
+
+  await app.listen(port);
+}
+bootstrap();
